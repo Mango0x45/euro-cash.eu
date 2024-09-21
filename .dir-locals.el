76022185d0767435a41f70760e71029c2a889d67
Thomas Voss <mail@thomasvoss.com>
2024-09-21 07:25:24 +0200

    Add a working .dir-locals.el for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((eval . (progn
+                   (add-to-list
+                    'load-path
+                    (expand-file-name
+                     "contrib/emacs"
+                     (locate-dominating-file default-directory "contrib/emacs")))
+                   (require 'mintage-mode))))))
