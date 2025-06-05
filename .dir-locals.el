169bceff8538bb985aa7b37171d1527c4cce4e0a
Thomas Voss <mail@thomasvoss.com>
2025-06-06 01:43:14 +0200

    Set ‘number-format-predicate’

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,9 @@
-((go-ts-mode . ((comment-start . "/* ")
+((csv-mode . ((number-format-predicate
+               . (lambda (beg end)
+                   (save-excursion
+                     (goto-char beg)
+                     (not (bolp)))))))
+ (go-ts-mode . ((comment-start . "/* ")
                 (comment-end . " */")
                 (comment-continue . "   ")
                 (require-final-newline . t)))
