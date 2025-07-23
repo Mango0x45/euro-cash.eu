74747c981660dc3e8e23480f8484db6d5f439df0
Thomas Voss <mail@thomasvoss.com>
2025-07-23 20:37:49 +0200

    Disable auto-fill-mode (properly I hope)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,13 @@
-((csv-mode . ((number-format-predicate
-               . (lambda (beg end)
-                   (save-excursion
-                     (goto-char beg)
-                     (not (bolp)))))))
- (go-ts-mode . ((comment-start . "/* ")
-                (comment-end . " */")
-                (comment-continue . "   ")
-                (require-final-newline . t)))
- (mhtml-mode . ((auto-fill-mode . nil))))
+((csv-mode
+  ((number-format-predicate
+    . (lambda (beg end)
+        (save-excursion
+          (goto-char beg)
+          (not (bolp)))))))
+ (go-ts-mode
+  ((comment-start . "/* ")
+   (comment-end . " */")
+   (comment-continue . "   ")
+   (require-final-newline . t)))
+ (mhtml-mode
+  ((eval . (add-hook 'mhtml-mode-hook (lambda () (auto-fill-mode -1)))))))
